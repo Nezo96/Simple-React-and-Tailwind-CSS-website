49bd7a1a65c80d6c40e7f6fe128f3fb96fbb972f
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-04-06 08:12:55 +0000

    Added responsiveness to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  //Navbar hide/show
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
-      
-      <ul className="flex">
+
+      <ul className="flex hidden">
         <a href="#" className="p-4">
           Home
         </a>
@@ -23,6 +30,32 @@ const Navbar = () => {
           Contact
         </a>
       </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+
+      {/* mobile menu */}
+      <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
+        <ul className="p-4 flex flex-col uppercase">
+          <a href="#" className="p-4 border-b border-gray-600">
+            Home
+          </a>
+          <a href="#" className="p-4 border-b border-gray-600">
+            Company
+          </a>
+          <a href="#" className="p-4 border-b border-gray-600">
+            Resource
+          </a>
+          <a href="#" className="p-4 border-b border-gray-600">
+            About
+          </a>
+          <a href="#" className="p-4">
+            Contact
+          </a>
+        </ul>
+      </div>
+      {/* mobile menu end */}
     </div>
   );
 };
