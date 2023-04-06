cbaf28b38c3cb8e104ad74fe5158875b6e75efef
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-04-06 09:42:35 +0000

    Added footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,21 @@ import {
 } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div>
+        <h1 className="uppercase w-full text-3xl font-bold text-[#00df9a]">
+          react.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
