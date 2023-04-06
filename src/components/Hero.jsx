a6ac4bfabd44dc23e0e2cc0fae5fd218247f4bf1
Nezo96 <39273050+Nezo96@users.noreply.github.com>
2023-04-06 08:30:12 +0000

    Added hero structure + react-typed package

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Typed from 'react-typed';
 
 const Hero = () => {
-  return <div className="text-white">Hero</div>;
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="uppercase text-[#00df9a] font-bold p-2">
+          growing with data analytics
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div>
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible financing for
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
